c5cf5294adb423940cf7068b11dfbe3386148bc6
SyroidNadiia <112964918+SyroidNadiia@users.noreply.github.com>
2024-02-09 10:02:52 +0200

    added ScrollToTopButton

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@components/Footer/Footer";
 import Header from "@components/Header/Header";
+import ScrollToTopButton from "@components/ScrollToTopButton/ScrollToTopButton";
 
 import "./globals.css";
 
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Header />
         <main className={styles.main}>{children}</main>
         <Footer />
+        <ScrollToTopButton />
       </body>
     </html>
   );
